9f17f63dcc111a8d1662f2dc100f67fce5793836
changhee.an <changhee.an@tridge.com>
2025-09-02 17:09:56 +0900

    [feat] Update formatter

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,76 +1,131 @@
 -- LSP configuration that will be loaded after plugins
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-      -- Use an on_attach function to only map the following keys
-      -- after the language server attaches to the current buffer
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-        callback = function(ev)
-          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-          local opts = { buffer = ev.buf }
-          vim.keymap.set("n", "gD", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "gK", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-          vim.keymap.set("n", "gk", vim.lsp.buf.signature_help, opts)
-          vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-          vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-          vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-          vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-          vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-          vim.keymap.set("n", "<space>f", function()
-            vim.lsp.buf.format({ async = true })
-          end, opts)
-        end,
-      })
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			-- Use an on_attach function to only map the following keys
+			-- after the language server attaches to the current buffer
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+					local opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gD", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "gK", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "gk", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+					vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+					vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					-- 포맷팅은 conform.nvim의 <space>f가 처리하므로 제거
+					-- vim.keymap.set("n", "<space>f", function()
+					--   vim.lsp.buf.format({ async = true })
+					-- end, opts)
+				end,
+			})
 
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      
-      -- Python
-      lspconfig["pyright"].setup({
-        capabilities = capabilities,
-        settings = {
-          python = {
-            analysis = {
-              diagnosticMode = "openFilesOnly",
-              typeCheckingMode = "off",
-            },
-          },
-        },
-      })
+			local lspconfig = require("lspconfig")
 
-      -- Lua
-      lspconfig["lua_ls"].setup({
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = {
-                "vim",
-              },
-            },
-          },
-        },
-      })
+			-- Python
+			lspconfig["pyright"].setup({
+				capabilities = capabilities,
+				settings = {
+					python = {
+						analysis = {
+							diagnosticMode = "openFilesOnly",
+							typeCheckingMode = "off",
+						},
+					},
+				},
+			})
 
-      -- Setup remaining LSP servers
-      local servers = { "taplo", "ts_ls", "yamlls" }
+			-- Lua
+			lspconfig["lua_ls"].setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = {
+								"vim",
+							},
+						},
+					},
+				},
+			})
 
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup({
-          capabilities = capabilities,
-        })
-      end
-    end,
-  },
+			-- ESLint LSP configuration
+			lspconfig["eslint"].setup({
+				capabilities = capabilities,
+				on_attach = function(client, bufnr)
+					-- ESLint의 포맷팅 기능 비활성화 (conform.nvim이 처리)
+					client.server_capabilities.documentFormattingProvider = false
+					client.server_capabilities.documentRangeFormattingProvider = false
+				end,
+				settings = {
+					validate = "on",
+					packageManager = "npm",
+					autoFixOnSave = false, -- conform.nvim이 처리하므로 비활성화
+					codeActionOnSave = {
+						enable = false, -- conform.nvim이 처리
+						mode = "all",
+					},
+					format = false, -- 포맷팅은 conform.nvim이 처리
+					quiet = false,
+					onIgnoredFiles = "off",
+					rulesCustomizations = {},
+					run = "onType",
+					problems = {
+						shortenToSingleLine = false,
+					},
+					-- 프로젝트의 ESLint 설정 자동 감지
+					workingDirectory = {
+						mode = "auto",
+					},
+				},
+			})
+
+			-- TypeScript/JavaScript LSP
+			lspconfig["ts_ls"].setup({
+				capabilities = capabilities,
+				on_attach = function(client, bufnr)
+					-- TypeScript 서버의 포맷팅 기능 비활성화 (conform.nvim이 처리)
+					client.server_capabilities.documentFormattingProvider = false
+					client.server_capabilities.documentRangeFormattingProvider = false
+				end,
+				settings = {
+					typescript = {
+						format = {
+							enable = false, -- conform.nvim이 처리
+						},
+					},
+					javascript = {
+						format = {
+							enable = false, -- conform.nvim이 처리
+						},
+					},
+				},
+			})
+
+			-- Setup remaining LSP servers
+			local servers = { "taplo", "yamlls" }
+
+			for _, server in ipairs(servers) do
+				lspconfig[server].setup({
+					capabilities = capabilities,
+				})
+			end
+		end,
+	},
 }
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,273 +1,373 @@
 return {
-  -- Theme (Solarized Light)
-  {
-    "ishan9299/nvim-solarized-lua",
-    config = function()
-      vim.cmd([[colorscheme solarized]])
-      vim.o.background = "light"
-    end,
-  },
+	-- Theme (Solarized Light)
+	{
+		"ishan9299/nvim-solarized-lua",
+		config = function()
+			vim.cmd([[colorscheme solarized]])
+			vim.o.background = "light"
+		end,
+	},
 
-  -- Essential UI
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "solarized_light",
-        },
-      })
-    end,
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("nvim-tree").setup({
-        actions = {
-          open_file = {
-            quit_on_open = true,
-          },
-        },
-      })
-    end,
-  },
-  "nvim-tree/nvim-web-devicons",
-  {
-    "akinsho/bufferline.nvim",
-    config = function()
-      require("bufferline").setup()
-    end,
-  },
+	-- Essential UI
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "solarized_light",
+				},
+			})
+		end,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("nvim-tree").setup({
+				actions = {
+					open_file = {
+						quit_on_open = true,
+					},
+				},
+			})
+		end,
+	},
+	"nvim-tree/nvim-web-devicons",
+	{
+		"akinsho/bufferline.nvim",
+		config = function()
+			require("bufferline").setup()
+		end,
+	},
 
-  -- Core functionality
-  "nvim-lua/plenary.nvim",
-  {
-    "folke/which-key.nvim",
-    config = function()
-      require("which-key").setup({})
-    end,
-  },
-  {
-    "numToStr/Comment.nvim",
-    config = function()
-      require('Comment').setup({
-        toggler = {
-          line = '<Leader>cc',
-        },
-        opleader = {
-          line = '<Leader>cc',
-        },
-      })
-    end,
-  },
-  "tpope/vim-surround",
-  "justinmk/vim-sneak",
-  {
-    "mbbill/undotree",
-    config = function()
-      vim.g.undotree_WindowLayout = 2
-      vim.g.undotree_SplitWidth = 30
-    end,
-  },
+	-- Core functionality
+	"nvim-lua/plenary.nvim",
+	{
+		"folke/which-key.nvim",
+		config = function()
+			require("which-key").setup({})
+		end,
+	},
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup({
+				toggler = {
+					line = "<Leader>cc",
+				},
+				opleader = {
+					line = "<Leader>cc",
+				},
+			})
+		end,
+	},
+	"tpope/vim-surround",
+	"justinmk/vim-sneak",
+	{
+		"mbbill/undotree",
+		config = function()
+			vim.g.undotree_WindowLayout = 2
+			vim.g.undotree_SplitWidth = 30
+		end,
+	},
 
-  -- File navigation
-  "junegunn/fzf",
-  "junegunn/fzf.vim",
+	-- File navigation
+	"junegunn/fzf",
+	"junegunn/fzf.vim",
 
-  -- LSP and completion
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  "neovim/nvim-lspconfig",
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "pyright",
-          "ts_ls",
-          "taplo",
-          "yamlls",
-        },
-        automatic_installation = true,
-      })
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "lua",
-          "python",
-          "javascript",
-          "typescript",
-          "yaml",
-          "json",
-          "markdown",
-        },
-        indent = {
-          enable = true,
-        },
-        highlight = {
-          enable = true,
-        },
-      })
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<CR>"] = cmp.mapping.confirm({ select = false }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }, {
-          { name = "buffer" },
-        }),
-      })
+	-- LSP and completion
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	"neovim/nvim-lspconfig",
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"pyright",
+					"ts_ls",
+					"eslint", -- ESLint LSP 추가
+					"taplo",
+					"yamlls",
+				},
+				automatic_installation = true,
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"lua",
+					"python",
+					"javascript",
+					"typescript",
+					"yaml",
+					"json",
+					"markdown",
+				},
+				indent = {
+					enable = true,
+				},
+				highlight = {
+					enable = true,
+				},
+			})
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
 
-      cmp.setup.cmdline({ "/", "?" }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          { name = "cmdline" },
-        }),
-      })
-    end,
-  },
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "L3MON4D3/LuaSnip",
-  "saadparwaiz1/cmp_luasnip",
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			})
+		end,
+	},
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
 
-  -- Git integration
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup({})
-    end,
-  },
-  "tpope/vim-fugitive",
+	-- Git integration
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({})
+		end,
+	},
+	"tpope/vim-fugitive",
 
-  -- Tmux integration
-  "alexghergh/nvim-tmux-navigation",
+	-- Tmux integration
+	"alexghergh/nvim-tmux-navigation",
 
-  -- Formatting with conform.nvim
-  {
-    "stevearc/conform.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-      {
-        "<space>f",
-        function()
-          require("conform").format({ async = true, lsp_fallback = true })
-        end,
-        mode = "",
-        desc = "Format buffer",
-      },
-    },
-    config = function()
-      require("conform").setup({
-        formatters_by_ft = {
-          lua = { "stylua" },
-          python = { "ruff_format", "ruff_organize_imports" },
-          javascript = { "eslint_d" },
-          typescript = { "eslint_d" },
-          javascriptreact = { "eslint_d" },
-          typescriptreact = { "eslint_d" },
-          sql = { "sqlfmt" },
-          json = { "prettier" },
-          yaml = { "prettier" },
-          markdown = { "prettier" },
-        },
-        format_on_save = {
-          timeout_ms = 5000,
-          lsp_fallback = false,
-        },
-      })
-    end,
-  },
+	-- Formatting with conform.nvim
+	{
+		"stevearc/conform.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<space>f",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		config = function()
+			-- Helper function to check if file exists
+			local function file_exists(name)
+				local f = io.open(name, "r")
+				if f ~= nil then
+					io.close(f)
+					return true
+				else
+					return false
+				end
+			end
 
-  -- Linting with nvim-lint
-  {
-    "mfussenegger/nvim-lint",
-    dependencies = { "williamboman/mason.nvim" },
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local lint = require("lint")
-      lint.linters_by_ft = {
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
-        python = { "ruff" },
-        -- SQLFluff는 레거시 - sqlfmt 사용
-        -- sql = {},  -- Linting 비활성화 (sqlfmt는 포맷터만 제공)
-      }
+			-- Helper function to check for ESLint config in project
+			local function has_eslint_config()
+				local config_files = {
+					".eslintrc.js",
+					".eslintrc.cjs",
+					".eslintrc.json",
+					".eslintrc.yml",
+					".eslintrc.yaml",
+					".eslintrc",
+					"eslint.config.js",
+					"eslint.config.mjs",
+					"eslint.config.cjs",
+				}
 
-      -- Create an autocommand to trigger linting
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-        callback = function()
-          lint.try_lint()
-        end,
-      })
-    end,
-  },
+				local cwd = vim.fn.getcwd()
+				for _, config_file in ipairs(config_files) do
+					if file_exists(cwd .. "/" .. config_file) then
+						return true
+					end
+				end
 
-  -- Mason integration for formatters and linters
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-tool-installer").setup({
-        ensure_installed = {
-          "stylua",
-          "eslint_d",
-          "prettier",
-          "ruff",
-          -- "sqlfluff",  -- 레거시, sqlfmt 사용
-        },
-        automatic_installation = true,
-      })
-    end,
-  },
+				-- Check for eslintConfig in package.json
+				if file_exists(cwd .. "/package.json") then
+					local package = vim.fn.readfile(cwd .. "/package.json")
+					local package_str = table.concat(package, "\n")
+					if string.find(package_str, '"eslintConfig"') then
+						return true
+					end
+				end
+
+				return false
+			end
+
+			-- Helper function to check for Prettier config in project
+			local function has_prettier_config()
+				local config_files = {
+					".prettierrc",
+					".prettierrc.json",
+					".prettierrc.yml",
+					".prettierrc.yaml",
+					".prettierrc.js",
+					".prettierrc.cjs",
+					".prettierrc.mjs",
+					"prettier.config.js",
+					"prettier.config.cjs",
+					"prettier.config.mjs",
+				}
+
+				local cwd = vim.fn.getcwd()
+				for _, config_file in ipairs(config_files) do
+					if file_exists(cwd .. "/" .. config_file) then
+						return true
+					end
+				end
+
+				-- Check for prettier config in package.json
+				if file_exists(cwd .. "/package.json") then
+					local package = vim.fn.readfile(cwd .. "/package.json")
+					local package_str = table.concat(package, "\n")
+					if string.find(package_str, '"prettier"') then
+						return true
+					end
+				end
+
+				return false
+			end
+
+			-- Dynamic formatter selection
+			local function get_js_formatters()
+				local formatters = {}
+
+				-- Check for project-specific configs
+				if has_eslint_config() then
+					-- ESLint가 있으면 우선 사용
+					table.insert(formatters, "eslint_d")
+				elseif has_prettier_config() then
+					-- ESLint가 없고 Prettier만 있으면 Prettier 사용
+					table.insert(formatters, "prettier")
+				else
+					-- 둘 다 없으면 기본값 (eslint_d)
+					table.insert(formatters, "eslint_d")
+				end
+
+				return formatters
+			end
+
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					python = { "ruff_format", "ruff_organize_imports" },
+					-- JavaScript/TypeScript: 동적으로 포맷터 선택
+					javascript = get_js_formatters,
+					typescript = get_js_formatters,
+					javascriptreact = get_js_formatters,
+					typescriptreact = get_js_formatters,
+					sql = { "sqlfmt" },
+					-- JSON/YAML/Markdown: 항상 prettier 사용
+					json = { "prettier" },
+					yaml = { "prettier" },
+					markdown = { "prettier" },
+				},
+				format_on_save = {
+					timeout_ms = 5000,
+					lsp_fallback = false,
+				},
+			})
+		end,
+	},
+
+	-- Linting with nvim-lint
+	{
+		"mfussenegger/nvim-lint",
+		dependencies = { "williamboman/mason.nvim" },
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local lint = require("lint")
+			lint.linters_by_ft = {
+				javascript = { "eslint_d" },
+				typescript = { "eslint_d" },
+				javascriptreact = { "eslint_d" },
+				typescriptreact = { "eslint_d" },
+				python = { "ruff" },
+				-- SQLFluff는 레거시 - sqlfmt 사용
+				-- sql = {},  -- Linting 비활성화 (sqlfmt는 포맷터만 제공)
+			}
+
+			-- Create an autocommand to trigger linting
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+		end,
+	},
+
+	-- Mason integration for formatters and linters
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"stylua",
+					"eslint_d",
+					"prettier",
+					"ruff",
+					-- "sqlfluff",  -- 레거시, sqlfmt 사용
+				},
+				automatic_installation = true,
+			})
+		end,
+	},
 }
